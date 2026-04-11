13175ca54193570d93216483127fb3cd77f559ba
garvjain7 <2023pietcrgarv018@poornima.org>
2026-04-12 04:12:42 +0530

    adding dependencies

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,5 +1,7 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
